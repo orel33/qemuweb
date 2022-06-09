9f7744b75239351018f4263e9a9fff3fd91af061
gbienfait <guillaume.bienfait@etu.u-bordeaux.fr>
2022-06-09 11:44:48 +0200

    première version select distributions

--- src/components/nodes/HostComp.vue
+++ src/components/nodes/HostComp.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+        <select>
+            <option v-for="item in distributions" :key="item.value" :label="item.label" :value="item.value"></option>
+        </select>
         <img src="../../assets/computer.png" />
         <input class="host-name" type="text" v-model="name">
     </div>
@@ -11,7 +14,46 @@ import { defineComponent, onMounted, getCurrentInstance, readonly, ref, nextTick
 export default defineComponent({
     data() {
         return {
-            name: "bob"
+            name: "bob",
+            distributions: [
+                {
+                    value: 'debian10',
+                    label: 'Debian 10'
+                },
+                {
+                    value: 'debian10x',
+                    label: 'Debian 10 X11'
+                },
+                {
+                    value: 'debian9',
+                    label: 'Debian 9'
+                },
+                {
+                    value: 'tinycore',
+                    label: 'Tinycore'
+                },
+                {
+                    value: 'alpine',
+                    label: 'Alpine'
+                },
+                {
+                    value: 'alpinex',
+                    label: 'Alpine X11'
+                },
+                {
+                    value: 'kali',
+                    label: 'Kali'
+                },
+                {
+                    value: 'windowsxp',
+                    label: 'Windows XP'
+                }
+            ]
+        }
+    },
+    methods: {
+        updateSelect(value) {
+            
         }
     }
 })
@@ -21,5 +63,11 @@ export default defineComponent({
 img {
     width:65px;
     height:65px;
+}
+select {
+    position: relative;
+    left: -20px;
+    opacity: 0.9;
+    border-radius: 35px;
 }
 </style>
